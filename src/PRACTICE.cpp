07f2a7027593a233654b13a3b9608b55c8807271
Prathmesh-Patil <prathmeshpatil2929@gmail.com>
2025-02-22 12:52:56 +0530

    EMbedkari

---
+++ src/PRACTICE.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : PRACTICE.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a=10;
+	int b=20;
+	cout << "A+B :" <<a+b<< endl; // prints !!!Hello World!!!
+	return 0;
+}
